a5f7125e1365d7d70a367165c9dc508ab8c40782
lli259 <37081135+lli259@users.noreply.github.com>
2021-10-26 08:22:10 -0400

    print information

--- feature_selection.py
+++ feature_selection.py
@@ -155,10 +155,12 @@ def select_f(args,performance_folder_group):
             feature_selected_num_min=int(len(feature_domain.columns)*0.4)
             feature_selected_num_max=int(len(feature_domain.columns)*0.7)            
         for diff_f_num in range(feature_selected_num_min,feature_selected_num_max+1):
+            print('Features Evaluation, 'diff_f_num)
             most_meaning_f_dm=get_most_meaningful(feature_domain,performance_data,diff_f_num)
             score=get_accuracy(most_meaning_f_dm,feature_domain,performance_data)
             all_diff_features.append((score,most_meaning_f_dm))
-
+            
+        print(all_diff_features)
         all_diff_features=sorted(all_diff_features)
         best_score=all_diff_features[0]       
         most_meaning_f_dm=best_score[1]
